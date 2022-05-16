8044a11a1c514064b248ac8d37e2d511021f2743
Laetitia DECRE <laetitia.decre@gmail.com>
2022-05-16 10:27:31 +0200

    creation page connexion

---
+++ src/header.php
@@ -0,0 +1,11 @@
+<header>
+<div class="menu">
+            <div class="logo">
+                <img src="IDE/images/logo.png" alt="">
+            </div>
+            <div class="connection">
+                <div class="login"></div>
+                <div class="register"></div>
+            </div>
+</div>
+</header>
